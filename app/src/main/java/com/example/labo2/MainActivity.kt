e4625f6859d28ae1a8ccb8ebe4efc134457fba3e
IvanaFD <ivanafigueroa0936@gmail.com>
2025-08-01 05:55:56 -0600

    document code

--- app/src/main/java/com/example/labo2/MainActivity.kt
+++ app/src/main/java/com/example/labo2/MainActivity.kt
@@ -1,50 +1,31 @@
 package com.example.labo2
 
+import androidx.compose.ui.tooling.preview.Preview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-
-import androidx.compose.ui.text.style.TextAlign
-
-import androidx.compose.ui.unit.dp
-import com.example.labo2.R
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
-
-
-import androidx.compose.ui.unit.dp
-
+//Configura la interfaz de usuario usando jetpack compose
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +37,9 @@ class MainActivity : ComponentActivity() {
 
     }
 }
+
+//Fucnion componente principal de la interfaz, estructura todos los elementos y modifica el formato
+//Muestra lso mensajes de bienvenida y boton con mensaje snackbar
 @Composable
 fun Window() {
     val snackbarHostState = remember { SnackbarHostState() }
@@ -90,7 +74,7 @@ fun Window() {
 }
 
 
-
+//Funcion que permite previzualizar la funci[on window sin necesidad de emular
 @Preview
 @Composable
 fun PreviewWindow(){
